7feee4adbb8d4f33be20735aef5bca658ea937ea
satendra <web.developer.sn@gmail.com>
2025-02-13 01:42:03 +0530

    after changing for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/image2pdf/' : '',
+  basePath: isProd ? '/image2pdf' : '',
+  output: 'export'
 };
 
 export default nextConfig;
