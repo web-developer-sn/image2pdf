104aa17768a3916d08cdd846ea9960692e27fea3
satendra <150445188+web-developer-sn@users.noreply.github.com>
2026-02-10 22:57:58 +0530

    Simplify next.config.ts by removing production checks

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? '/image2pdf/' : '',
-  basePath: isProd ? '/image2pdf' : ''
+  images: { unoptimized: true },
+  assetPrefix: '',
+  basePath: ''
 };
 
 export default nextConfig;
